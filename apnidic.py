c6f8ac4df2d33f4685db0254daf7e4ce4caa4d09
Phoenix_Zura <62949813+Phoenix-Zura@users.noreply.github.com>
2020-08-22 15:12:20 +0530

    Add files via upload

---
+++ apnidic.py
@@ -0,0 +1,4 @@
+D = {"set":"To arrange","abonden":"To leave behind","cricket":"An african insect","level":"Height"}
+print("Enter the word:")
+userip = input()
+print("The meaning of", userip,"is",D[userip])
