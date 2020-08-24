d3550195bbdc0899b3330c4821962a3d61a362be
Phoenix_Zura <62949813+Phoenix-Zura@users.noreply.github.com>
2020-08-24 11:37:38 +0530

    Add files via upload

---
+++ license.py
@@ -0,0 +1,8 @@
+print("Enter your age:")
+age = int(input())
+if age > 18 and age <= 100:
+    print("you are eligable")
+elif age == 18:
+    print("Make a direct visit")
+else:
+    print("You are not eligable")
