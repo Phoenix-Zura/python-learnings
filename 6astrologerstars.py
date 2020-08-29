3e73f26c029e1486677c41f0766ef1180d0edaa1
Phoenix_Zura <62949813+Phoenix-Zura@users.noreply.github.com>
2020-08-29 09:16:19 +0530

    Update 6astrologerstars.py

--- 6astrologerstars.py
+++ 6astrologerstars.py
@@ -1,22 +1,26 @@
-n = int(input("Enter the number of rows:"))
-c = int(input("Enter 1(normal) or 0(inverted):"))
-c1 = bool(c)
-row = 1
-n1 = n
-if c1:
-    while(row <= n):
-        itr = 1
-        while(itr <= row):
-            print("*", end="")
-            itr += 1
-        print()
-        row += 1
-else:
-    while (row <= n):
-        itr = 1
-        while (itr <= n1):
-            print("*", end="")
-            itr += 1
-        print()
-        row += 1
-        n1 -= 1
+try:
+    n = int(input("Enter the number of rows:"))
+    c = int(input("Enter 1(normal) or 0(inverted):"))
+    c1 = bool(c)
+    row = 1
+    n1 = n
+    if c1:
+        while(row <= n):
+            itr = 1
+            while(itr <= row):
+                print("*", end="")
+                itr += 1
+            print()
+            row += 1
+    else:
+        while (row <= n):
+            itr = 1
+            while (itr <= n1):
+                print("*", end="")
+                itr += 1
+            print()
+            row += 1
+            n1 -= 1
+except Exception as e:
+    print("invalid input")
+    
